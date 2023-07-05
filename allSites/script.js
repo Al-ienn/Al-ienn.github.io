e97c3cab090e55b197e1d51e92476e3510787fdd
PedaJ <jagapeda12@gmail.com>
2023-07-05 19:39:39 +0200

    commitTwo

---
+++ allSites/script.js
@@ -0,0 +1,6 @@
+function mode(){
+    var element = document.body;
+    element.classList.toggle("dark");
+    var arti = document.getElementById("articleDiv");
+    arti.classList.toggle("dark");
+  }
